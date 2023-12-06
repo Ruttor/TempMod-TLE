eec9353762c012a22625e58d0cfe06287c277349
Ruttor <fabianfelden.ff@gmail.com>
2023-12-06 16:45:13 +0100

    correct color map plotting, clean up code

--- sol.py
+++ sol.py
@@ -18,93 +18,150 @@ params = {
     'thick0': 0.0,
     }
 
-def schichtdicke(P, t_end=100, t_p=None, n=10, pulsed=False):
-
-    # erhebt Valuerror 
-    # wenn t_p nicht angegeben ist, aber gepulster Betrieb gewünscht ist
+def thickness(P, t_end=100, t_p=None, n=10, pulsed=False):
+    """Calculates the thickness of the grown layer and the maximum substrate temperature.
+    
+    Parameters
+    ----------
+    P : float
+        Laser power
+    t_end : float
+        End time (default: 100)
+    t_p : float
+        Pulse duration (required if pulsed is True)
+    n : int
+        Number of pulses (required if pulsed is True)
+    pulsed : bool
+        Whether it's pulsed laser heating (default: False)
+    
+    Returns
+    -------
+    tuple: A tuple containing the thickness of the grown layer and the maximum substrate temperature.
+    """
+    # raises Valuerror if t_p is not given, but pulsed is True
     if pulsed and not t_p:
         raise ValueError("t_p is required for pulsed laser heating.")
     
-    tm = mod.TempMod(dgl.T_punkt)  # Initialisiert die Temperaturmodell-Klasse
+    tm = mod.TempMod(dgl.T_punkt) # Initializes the TempMod class
     if pulsed:
         result = tm.mod(P=P, T0_so=params['T0_so'], T0_sub=params['T0_sub'], pulsed=pulsed, f=params['f'], t_p=t_p, n=n, gr_factor=dgl.c['gr_factor'], gr_exp=dgl.c['gr_exp'])
     else:
         result = tm.mod(P=P, T0_so=params['T0_so'], T0_sub=params['T0_sub'], t_range=[0, t_end], gr_factor=dgl.c['gr_factor'], gr_exp=dgl.c['gr_exp'])
-    # Returnt die letzte Schichtdicke und die maximale Substrattemperatur
+    # Returns the last thickness and the maximum substrate temperature
     return result['Thickness'][-1], result['Temp_substrate'].max()
 
-def fehlerfunktion(P_pulse, t_p, rate, t_end, n, cache={}):
-
-    # Schlüssel für den Cache
-    key = (t_p, rate, P_pulse, t_end, n)
-    if key in cache:
-        return cache[key]  # Gibt gecachtes Ergebnis zurück, falls vorhanden
-    # Berechnet nur die Differenz der Schichtdicken, die Temperatur wird hier ignoriert
+def difference(P_pulse, t_p, rate, t_end, n):
+    """Calculates the difference between the thickness of the grown layer for constant and pulsed laser heating.
+    
+    Parameters
+    ----------
+    P_pulse : float
+        Laser power
+    t_p : float
+        Pulse duration
+    rate : float
+        Rate of the laser heating
+    t_end : float
+        End time
+    n : int
+        Number of pulses
+    cache : dict
+        Cache for the results (default: {})
+    
+    Returns
+    -------
+    float: The difference between the thickness of the grown layer for constant and pulsed laser heating.
+    """
+    # Calculates the thickness of the grown layer for constant laser heating
     thickness_const = rate * t_end
-    thickness_pulse, _ = schichtdicke(P_pulse, t_end, t_p, n, pulsed=True)
-    #print(thickness_pulse)
+    thickness_pulse, _ = thickness(P_pulse, t_end, t_p, n, pulsed=True)
     diff = thickness_const - thickness_pulse
-    cache[key] = diff
     return diff
 
 
-def nullstellen(func, a, b, args=()):
-    bisektion = bisect(func, a, b, args=args, xtol=1)
-    nullstelle = newton(func, bisektion, args=args, tol=0.1)
-    print(nullstelle)
-    return nullstelle
+def find_power(func, b, args=()):
+    """Calculates the points of a function where it crosses the x-axis (optimal power).
+    
+    Parameters
+    ----------
+    func : function
+        Function which varies the power in order to reduce the difference of thickness between constant and pulsed laser heating
+    b : float
+        Upper bound for the power
+    args : tuple
+        Arguments for the function (default: ())
+    
+    Returns
+    -------
+    float: The optimal power
+    """
+    bisection = bisect(func, 0, b, args=args, xtol=1)
+    opt_power = newton(func, bisection, args=args, tol=0.01)
+    return opt_power
 
-if __name__ == "__main__":
-    # Beispiel für die Anwendung der geänderten Funktion
-    n = 10
-    t_p = [i*0.1 for i in range(1, n+1)]
-    rate_vals = [i*0.1 for i in range(1, n+1)]
+def calc_p_opt(t_p, rate_vals):
+    """Calculates the optimal power for different pulse durations and rates.
+    
+    Parameters
+    ----------
+    t_p : list
+        List of pulse durations
+    rate_vals : list
+        List of rates
+    
+    Returns
+    -------
+    array: Array containing the optimal power for different pulse durations and rates
+    """
     p_opt = []
-    colormap_data = []
-    colormap_data_thick = []
-    print(t_p[0], rate_vals[0])
-
-
-
-    # Schleife für die Berechnung der optimalen Pulsbreite
-
-    print('p_opt Berechnung läuft:')
-    for i in range(len(rate_vals)):
-        print(f'Berechnung für t_p={t_p[i]}s:')
+    print('Calculation of optimal powervalues is running:')
+    for tp0 in t_p:
+        print(f'Calculation for t_p={tp0:.1f} s:')
         for r in rate_vals:
-            p_opt.append(nullstellen(fehlerfunktion, 0, 1e10, args=(t_p[i], r, 10, 10)))
-    print('p_opt Berechnung ist fertig.')
-    #print(p_opt)
-    # Schleife über die Leistung und Pulsbreiten
-    print('Berechnung der Temperaturen läuft:')
-    for tp in t_p:
+            p_opt.append(find_power(difference, 1e3, args=(tp0, r, 100, 10)))
+    p_opt = np.array(p_opt).reshape(len(t_p), len(rate_vals))
+    print('Calculation of optimal powervalues is done.')
+    return p_opt
+    
+def calc_temp(t_p, rate_vals, p_opt):
+    """Calculates the temperatures for different pulse durations and rates.
+    
+    Parameters
+    ----------
+    t_p : list
+        List of pulse durations
+    rate_vals : list
+        List of rates
+    p_opt : array
+        Array containing the optimal power for different pulse durations and rates
+    
+    Returns
+    -------
+    tuple: Tuple containing the temperatures and the thicknesses for different pulse durations and rates
+    """
+    colormap_data = []
+    colormap_data_thick = []    
+    print('Calculation of temperatures is running:')
+    for i in range(len(t_p)):
         for p in range(len(rate_vals)):
-            #print(p, tp)
-            thickness_solved, temperature = schichtdicke(P=p_opt[p], t_p=tp, pulsed=True)  # Gepulster Betrieb
-            print(f'Thickness: {thickness_solved}nm, Temperature: {temperature}K')
+            thickness_solved, temperature = thickness(P=p_opt[i][p], t_p=t_p[i], pulsed=True)
+            print(f'Thickness: {thickness_solved:.2f} A, Temperature: {temperature:.2f} K')
             colormap_data.append(temperature)
             colormap_data_thick.append(thickness_solved)
-    print('Berechnung der Temperaturen ist fertig.')
+    print('Calculation of temperatures is done.')
+    return colormap_data, colormap_data_thick
 
-    # Erstellen der Farbkarte
-    def create_colormap(rate, pulse_durations, temp):
-        # Erstellen des Meshgrids für die Leistung und die Pulsbreiten
-        p, T_p = np.meshgrid(rate, pulse_durations)
-        # Konvertiert die Temperaturen in ein Array und formt es in die richtige Form (2D)
-        Temps = np.array(temp).reshape(len(rate), len(pulse_durations))
-        # Erstellen der Farbkarte mit imshow
-        plt.figure(figsize=(8, 6))
-        plt.imshow(Temps, extent=(p.min(), p.max(), T_p.min(), T_p.max()), origin='lower', aspect='auto', cmap='inferno')
-        #for i in range(len(p_opt)):
-            #plt.plot(power, p_opt[i], ':', label=f'$t_P$ same thickness as const P={(i+10)*100 }W')
-        plt.colorbar(label='Substrattemperatur')
-        #plt.loglog()
-        plt.xlabel('Rate')
-        plt.ylabel('Pulsdauer')
-        plt.title('Temperatur Colormap')
-        #plt.legend()
-        plt.show()
-
-    create_colormap(rate_vals, t_p, colormap_data)
-
-        
+def create_colormap(rate, pulse_durations, temp):
+    # Creates a meshgrid for the colormap
+    p, T_p = np.meshgrid(rate, pulse_durations)
+    # Converts the temperatures into an array and reshapes it into the right shape (2D)
+    Temps = np.array(temp).reshape(len(rate), len(pulse_durations))
+    # Creates the colormap
+    plt.figure(figsize=(8, 6))
+    plt.imshow(Temps, extent=(p.min(), p.max(), T_p.min(), T_p.max()), origin='lower', aspect='auto', cmap='inferno')
+    # currently german labels because I need them for my thesis (will be changed in the future)
+    plt.colorbar(label='Substrattemperatur')
+    plt.xlabel('Rate')
+    plt.ylabel('Pulsdauer')
+    plt.title('Temperatur Colormap')
+    plt.show()
--- test_sol.py
+++ test_sol.py
@@ -1,34 +1,34 @@
 from sol import nullstellen
-
+import numpy as np
 def test_nullstellen():
     # Test case 1: Function becomes zero at x = 0
     def f(x):
         return x
 
-    result = nullstellen(f, -10, 10)
-    assert result == 0, "Test case 1 failed"
+    result = nullstellen(f, -1000, 1000)
+    np.testing.assert_allclose(result, 0, rtol=1e-03, err_msg='Test case 1 failed', verbose=True)
 
     # Test case 2: Function becomes zero at x = 5
     def g(x):
         return x - 5
 
-    result = nullstellen(g, 0, 10)
-    assert result == 5, "Test case 2 failed"
+    result = nullstellen(g, -1000, 1000)
+    np.testing.assert_allclose(result, 5, rtol=1e-03, err_msg='Test case 2 failed', verbose=True)
 
     # Test case 3: Function becomes zero at x = -3
     def h(x):
         return x + 3
 
-    result = nullstellen(h, -10, 0)
-    assert result == -3, "Test case 3 failed"
+    result = nullstellen(h, -1000, 1000)
+    np.testing.assert_allclose(result, -3, rtol=1e-03, err_msg='Test case 3 failed', verbose=True)
 
     
-    # Test case 4: Function becomes zero at x = 4000
+    # Test case 4: Function becomes zero at x = 2
     def i(x):
         return x**2 - 4*x + 4 
     
-    result = nullstellen(i, 0, 5)
-    assert result == 2, "Test case 4 failed"
+    result = nullstellen(i, -1000, 1000)
+    np.testing.assert_allclose(result, 2, rtol=1e-03, err_msg='Test case 4 failed', verbose=True)
     
     print("All test cases passed")
     
